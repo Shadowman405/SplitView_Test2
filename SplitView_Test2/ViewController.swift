806e24df117ec72b670e6258fa796e71bb007e8c
Maxim Mitin <maximus405@gmail.com>
2021-12-11 23:31:52 +0300

    Initial Commit

---
+++ SplitView_Test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitView_Test2
+//
+//  Created by Maxim Mitin on 11.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
